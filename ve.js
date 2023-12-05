101d04647984b7f43ffdc9c2115ce63f952a75de
rinrab <tima@chemodax.net>
2023-12-05 15:01:32 +0100

    Измѣняемъ ящерскому дiалѣкту съ славянскимъ.

---
+++ ve.js
@@ -0,0 +1,16 @@
+String.prototype.замѣнiть = String.prototype.replace;
+HTMLElement.prototype.ящерскоеПолѣ = HTMLElement.prototype.innerHTML;
+HTMLElement.prototype.ящерскоеПолѣ = HTMLElement.prototype.innerHTML;
+
+const русь = { };
+русь.молвить = console.log;
+
+const странiца = {};
+странiца.найтиЯщера = document.querySelector
+странiца.найтиВсехЯщеров = document.querySelectorAll
+
+const ЛовитѣльЯщеров = RegExp;
+
+const укротитѣльЯщеровъ = {}
+укротитѣльЯщеровъ.укротить = hljs.highlight;
+
